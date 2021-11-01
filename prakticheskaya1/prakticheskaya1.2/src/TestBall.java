af652ba783f942520cb1045ee94f61966fb25660
MaltsevVK <93158064+MaltsevVK@users.noreply.github.com>
2021-11-01 14:57:54 +0300

    Delete prakticheskaya1 directory

--- prakticheskaya1/prakticheskaya1.2/src/TestBall.java
+++
@@ -1,8 +0,0 @@
-public class TestBall {
-    public static void main(String[] args) {
-        Ball a1 = new Ball (50, 50);
-        System.out.println(a1);
-        a1.move(14, 7);
-        System.out.println(a1);
-    }
-}
